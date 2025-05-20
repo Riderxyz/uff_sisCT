ba84cc2a43dc1d605e82c2065375940b335ebe91
Iago <ifavilla@gmail.com>
2025-05-20 09:48:52 -0300

    .

--- sisCT/src/app/pages/sis-ct-cadastro/sis-ct-cadastro.component.ts
+++ sisCT/src/app/pages/sis-ct-cadastro/sis-ct-cadastro.component.ts
@@ -11,29 +11,7 @@ export class SisCtCadastroComponent {
   perguntaSelecionadaId: string = 'PERGUNTA_2';
 
   // No seu componente pai
-  listaDePerguntas: Pergunta[] = [
-    {
-      nome: 'Quais linguagens você conhece?',
-      id: 'PERGUNTA_1',
-      direcao: 'V',
-      respostas: [
-        { descricao: 'JavaScrwewqrewrewipt', id: 1, selecionado: false },
-        { descricao: 'TypeScriptX', id: 2, selecionado: false },
-        { descricao: 'Python', id: 3, selecionado: false },
-        { descricao: 'Java', id: 4, selecionado: false },
-      ],
-    },
-    {
-      nome: 'Quais frameworks front-end você usa?',
-      id: 'PERGUNTA_2',
-      direcao: 'V',
-      respostas: [
-        { descricao: 'Angular', id: 1, selecionado: false },
-        { descricao: 'React', id: 2, selecionado: false },
-        { descricao: 'Vue', id: 3, selecionado: false },
-      ],
-    },
-  ];
+
 
   subsectionArrForSection1 = [
     {
